9ed5f6a229ccb9c8be32546591a0b3bef7dc7cf1
tommie-sb <tommie.yeo@gmail.com>
2024-02-06 19:12:04 +0000

    join and split

---
+++ lab_input/challenge01.py
@@ -0,0 +1,18 @@
+#name = input("What is your name?")
+#ask the user their name
+
+#day = input("What is the day of the week?")
+
+#print = (Hello,"name" Happy "day")
+
+# Input asking for the user's name
+user_name = input("Please enter your name: ")
+
+# Input asking for the day of the week
+day_of_week = input("What day of the week is it today? ")
+
+# Printing out the user's name and the day of the week
+print("Hello,", user_name + "! Today is", day_of_week + ".")
+
+
+
