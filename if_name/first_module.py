9c2695b59e0fd5956d0de077a9627e44cb91ee63
tommie-sb <tommie.yeo@gmail.com>
2024-02-08 21:54:31 +0000

    importing main function

---
+++ if_name/first_module.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+print("This coddddddde will always execute.")
+
+def main():
+    print("this code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+    main()
---
+++ if_name/second_module.py
@@ -0,0 +1,7 @@
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+first_module.main() #this code is supposed to do something cool.... 
+
+print("Module #2 Name=", __name__)
